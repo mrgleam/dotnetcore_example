069cbc66896cefd00791a73ad7463e2ed63d9148
l54n71 <l.54n71@gmail.com>
2016-07-01 15:01:44 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace aspnetcoreapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseUrls("http://0.0.0.0:5000")
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+            host.Run();
+        }
+    }
+}
